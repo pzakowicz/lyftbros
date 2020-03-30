2c411defd0c7fad0ac992e000fe9ed0598bc16c7
Piotr Z <piotr.zakowicz@gmail.com>
2020-03-30 14:27:14 +0200

    bla

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let users = [];
+
+
 class User {
-    
+    constructor(name, surname, gender, email, password) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.email = email;
+        this.password = password;
+    }
 }
+
+
+function createUser() {
+    let user = {};
+    user.name = document.getElementById('name').value;
+    user.surname = document.getElementById('surname').value;
+    user.email = document.getElementById('email-create').value;
+    user.password = document.getElementById('password-create').value;
+    users.push(user);
+    console.log(users);
+}
+
+function printUser() {
+    document.getElementById('created-user').innerHTML = user.name;
+}
+
+// listeners
+
+let createUserButton = document.getElementById('create-user');
