ded1da4096b80a0f60891892f2b4d8e7573979bd
Piotr Z <piotr.zakowicz@gmail.com>
2020-04-04 13:47:44 +0200

    added change workout name feature

--- static/training.js
+++ static/training.js
@@ -5,6 +5,11 @@ const addSetButton = document.getElementById("submit-set-button");
 const workoutTableContainer = document.getElementById("workout-table-container");
 const workoutTable = document.getElementById("workout-table");
 const exerciseDropdown = document.getElementById("lyft");
+const workoutOwnerDropdown = document.getElementById("workout-owner");
+const workoutName = document.getElementById("workout-name");
+const changeWorkoutNameButton = document.getElementById("change-workout-name-button");
+const saveWorkoutNameButton = document.getElementById("save-workout-name-button");
+const changeWorkoutNameInput = document.getElementById("workout-name-input");
 
 // get pages
 exercisesButton.addEventListener('click', () => {
@@ -30,7 +35,24 @@ function getAllExercises() {
       exerciseDropdown.appendChild(newExercise);
     });
   });
-}
+};
+
+//GET all users and add them to dropdown as options
+function getAllUsers() {
+  fetch("/api/users")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    let userArray = data.user;
+    workoutOwnerDropdown.innerHTML = "";
+    userArray.forEach(element => {
+      const newUser = document.createElement("option");
+      newUser.innerHTML = `${element.first_name} ${element.surname}`; 
+      workoutOwnerDropdown.appendChild(newUser);
+    });
+  });
+};
 
 
 // add set to workout
@@ -47,10 +69,31 @@ addSetButton.addEventListener("click", () => {
   workoutTable.appendChild(newSet);
 });
 
+//change workout name
+changeWorkoutNameButton.addEventListener("click", () => {
+  changeWorkoutNameInput.style.display = "inline";
+  changeWorkoutNameButton.style.display = "none";
+  workoutName.style.display = "none";
+  saveWorkoutNameButton.style.display = "inline";
+})
+
+//save new workout name
+saveWorkoutNameButton.addEventListener("click", () => {
+  if (changeWorkoutNameInput.value) {
+    workoutName.innerHTML = changeWorkoutNameInput.value;
+    changeWorkoutNameInput.style.display = "none";
+    changeWorkoutNameButton.style.display = "";
+    workoutName.style.display = "";
+    saveWorkoutNameButton.style.display = "none";
+  }
+})
+
+
 
 //main function
 function main() {
   getAllExercises();
+  getAllUsers();
 };
 
 main();
