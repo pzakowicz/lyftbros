13faf82024e877a287801fd41c0f730c2619dbfb
Piotr Z <piotr.zakowicz@gmail.com>
2020-04-06 12:24:48 +0200

    created sets table

--- create-db.js
+++ create-db.js
@@ -12,5 +12,5 @@ db.run("CREATE TABLE IF NOT EXISTS Lifts (id INTEGER PRIMARY KEY, name TEXT NOT 
 db.run("CREATE TABLE IF NOT EXISTS Workouts(id INTEGER PRIMARY KEY, name TEXT NOT NULL, date_time DATE DEFAULT (datetime('now','localtime')), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES Users(id))");
 
 //Create Sets Table
-db.run("CREATE TABLE IF NOT EXISTS Sets(id INTEGER PRIMARY KEY, exercise_id INTEGER NOT NULL, weight INTEGER NOT NULL, reps INTEGER NOT NULL, workout_id INTEGER NOT NULL, FOREIGN KEY(exercise_id) REFERENCES Lifts(id), FOREIGN KEY(workout_id) REFERENCES Workouts(id)");
+db.run("CREATE TABLE IF NOT EXISTS Sets(id INTEGER PRIMARY KEY, exercise_id INTEGER NOT NULL, weight INTEGER NOT NULL, reps INTEGER NOT NULL, workout_id INTEGER NOT NULL, FOREIGN KEY(exercise_id) REFERENCES Lifts(id), FOREIGN KEY(workout_id) REFERENCES Workouts(id))");
 
--- sql queries.sql
+++ sql queries.sql
@@ -4,7 +4,7 @@
 --INSERT INTO Users (first_name, surname, gender, email, password) VALUES ("Piotr", "Zakowicz", "male", "piotr.zakowicz@gmail.com", "test")
 --INSERT INTO Lifts (name, category, body_part, type) VALUES ("Bench press", "Barbell", "Chest", "Weight & reps")
 --INSERT INTO Workouts (name, user_id) VALUES ("Lyft session", 1);
-CREATE TABLE IF NOT EXISTS Lifts (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, measurement TEXT NOT NULL)
+--CREATE TABLE IF NOT EXISTS Lifts (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, measurement TEXT NOT NULL)
 --CREATE TABLE IF NOT EXISTS Workouts(id INTEGER PRIMARY KEY, name TEXT NOT NULL, date_time DATE DEFAULT (datetime('now','localtime')),	user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES Users(id));
---DELETE FROM Lifts WHERE id=2
+DELETE FROM Lifts
 --DROP TABLE Lifts
