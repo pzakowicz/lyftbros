8fa7e76a8a0a813f9c8af2cd8abf5bb002479e76
Piotr Z <piotr.zakowicz@gmail.com>
2020-04-24 11:56:41 +0200

    changed config to heroku db

--- data/db-config.js
+++ data/db-config.js
@@ -1,9 +1,9 @@
   let config = {
-    host: 'localhost',
+    host: 'eu-cdbr-west-03.cleardb.net',
     port: 3306,
-    user: 'root',
-    password: 'R@GQ4JlHg5Qr@sI^',
-    database: 'lyftbros-db'
+    user: 'b2adf4419db6f2',
+    password: '079f2cd3',
+    database: 'heroku_e8d9557eb319104'
   };
   
   module.exports = config;
