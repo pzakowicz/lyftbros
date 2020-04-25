67dbcfc038c496c9c1ab1eba4bce22d45caac1b9
Piotr Z <piotr.zakowicz@gmail.com>
2020-04-25 14:20:38 +0200

    added environment log

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.get("/logout", (req, res) => {
     
 //starting app
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT} in ${process.env.NODE_ENV} environment.`);
 });
 
 //export
