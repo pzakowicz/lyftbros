2be18ff5c0a56e229c49b3308344dbc8db968cb3
Piotr Z <piotr.zakowicz@gmail.com>
2020-04-25 12:17:39 +0200

    added dotenv

--- data/db-config-cloud.js
+++ data/db-config-cloud.js
@@ -1,3 +1,4 @@
+  
   let config = {
     host: 'eu-cdbr-west-03.cleardb.net',
     port: 3306,
@@ -7,3 +8,38 @@
   };
   
   module.exports = config;
+  
+
+  /*
+  module.exports = function config(){
+    switch(process.env.NODE_ENV){
+        case 'development':
+            return {
+              host: 'localhost',
+              port: 3306,
+              user: 'root',
+              password: 'R@GQ4JlHg5Qr@sI^',
+              database: 'lyftbros-db'
+            };
+
+        case 'production':
+            return {
+            host: 'eu-cdbr-west-03.cleardb.net',
+            port: 3306,
+            user: 'b2adf4419db6f2',
+            password: '079f2cd3',
+            database: 'heroku_e8d9557eb319104'
+          };
+
+        default:
+            return {              
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: 'R@GQ4JlHg5Qr@sI^',
+            database: 'lyftbros-db'
+          };
+    }
+};
+
+*/
